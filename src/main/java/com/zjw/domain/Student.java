a4c46b3d9d77515e5d081bf2a112330a18f1e863
朱俊伟 <2585449492@qq.com>
2019-08-13 22:45:14 +0800

    2019-08-13 在Student中打印HelloWorld

--- src/main/java/com/zjw/domain/Student.java
+++ src/main/java/com/zjw/domain/Student.java
@@ -26,6 +26,7 @@ public class Student extends People {
 
 	@Override
 	public String toString() {
+		System.out.println("Hello World!");
 		return "Student [grade=" + grade + " name= " + super.getName() +"]";
 	}
 
